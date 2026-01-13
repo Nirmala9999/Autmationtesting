3534ea997ad4faef2bbdcda54ca5f0a159e5d811
Nirmala Reddy <Nirmala Reddy@DESKTOP-ILIHS4G>
2026-01-13 15:10:29 +0530

    commited

--- src/test/java/practice/DemoTest1.java
+++ src/test/java/practice/DemoTest1.java
@@ -8,14 +8,11 @@ public class DemoTest1
 	@Test(groups="Regression")
 	public void HarryPorterTest1()
 	{
-<<<<<<< HEAD
 		Reporter.log("Harmonine");
 		Reporter.log("Ron");
 		Reporter.log("Ginny");
-=======
 		Reporter.log("Harmonine");
 		Reporter.log("Hagrid");
 		Reporter.log("Ron");
->>>>>>> branch 'master' of https://github.com/Nirmala9999/Autmationtesting.git
 	}
 }
